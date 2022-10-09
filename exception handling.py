882c87dcfffae103e0002137dbb8dd66c7320861
Jaymin Lathiya <93418767+Jaymin-Lathiya@users.noreply.github.com>
2022-10-09 09:53:37 +0530

    Update exception handling.py

--- exception handling.py
+++ exception handling.py
@@ -89,3 +89,11 @@ except ZeroDivisionError:
     print("ZeroDivisionError Occurred and Handled")
 except NameError:
     print("NameError Occurred and Handled")
+
+#ex-7
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
